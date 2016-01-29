48d614e78e24596b22e21938e40004663ade011d
Devin Lehmacher <lehmacdj@gmail.com>
2016-01-28 23:03:48 -0500

    Minor changes + git aliases

--- install.d/brew.sh
+++ install.d/brew.sh
@@ -5,12 +5,13 @@
 echo -e "\nInstalling xcode commandline tools..."
 xcode-select --install
 
-echo -e "\nBrewing formulas..."
 
 # Install homebrew if nescessary
 brew help >/dev/null 2>&1 || \
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo -e "\nBrewing formulas..."
+
 # Use brew-bundle to install brewfiles in a good order
-brew tap Homebrew/bundle
+brew tap Homebrew/bundle 2>/dev/null
 brew bundle --file="~/.dotfiles/Brewfile"
