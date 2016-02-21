d1334f22df38a281da4e43e2b0d69e7ffd7dfd93
Devin Lehmacher <lehmacdj@gmail.com>
2016-02-20 22:18:11 -0500

    Remove sbg, modify zsh stuff

--- zsh.symdot/antigen.zsh
+++ zsh.symdot/antigen.zsh
@@ -1,16 +1,18 @@
 source /usr/local/share/antigen.zsh
 
-antigen use oh-my-zsh
-
 # Bundles
 antigen bundle git
 antigen bundle command-not-found
+antigen bundle brew
+antigen bundle brew-cask
+antigen bundle man
 
 # Syntax
 antigen bundle zsh-users/zsh-syntax-highlighting
 
 # Theme
 antigen theme agnoster
+source ~/.zsh/fix.zsh
 
 # Apply settings
 antigen apply
