623219d7099001bc018ced882ac6c376abda7b16
Devin Lehmacher <lehmacdj@gmail.com>
2016-02-22 14:32:40 -0500

    Remove excess whitespace, get rid of CDPATH

--- zsh.symdot/env.zsh
+++ zsh.symdot/env.zsh
@@ -12,7 +12,4 @@ if [ -f "/usr/libexec/java_home" ]; then
     export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 fi
 
-# CDPATH
-export CDPATH='~/.a'
-
 export HOMEBREW_CASK_OPTS='--appdir=/Applications'
