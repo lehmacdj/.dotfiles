bdd02b988c509e179489db2ef334e9aaba601daf
Devin Lehmacher <lehmacdj@gmail.com>
2016-02-24 17:28:49 -0500

    Add function for recursively doing git commands

--- zsh.symdot/aliases.zsh
+++ zsh.symdot/aliases.zsh
@@ -47,6 +47,37 @@ function swap () {
     mv $TMPFILE "$2"
 }
 
+# Recursively traverse directory tree for git repositories, run git command
+# e.g.
+#   gittree status
+#   gittree diff
+gittree() {
+  if [ $# -lt 1 ]; then
+    echo "Usage: gittree <command>"
+    return 1
+  fi
+
+  for gitdir in $(find . -type d -name .git); do
+    # Display repository name in blue
+    repo=$(dirname $gitdir)
+    echo -e "\033[34m$repo\033[0m"
+
+    # Run git command in the repositories directory
+    cd $repo && git $@
+    ret=$?
+
+    # Return to calling directory (ignore output)
+    cd - > /dev/null
+
+    # Abort if cd or git command fails
+    if [ $ret -ne 0 ]; then
+      return 1
+    fi
+
+    echo
+  done
+}
+
 if [ -f "$HOME/.aliases.local" ]; then
     source "$HOME/.aliases.local"
 fi
