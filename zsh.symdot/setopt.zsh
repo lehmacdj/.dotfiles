2c865219f8445b506e878d40ad6666e22a118bb6
Devin Lehmacher <lehmacdj@gmail.com>
2016-03-19 21:04:48 -0400

    Move history settings to history.zsh

--- zsh.symdot/setopt.zsh
+++ zsh.symdot/setopt.zsh
@@ -6,26 +6,6 @@ setopt AUTO_CD
 # Allow comments even in interactive shells
 setopt INTERACTIVE_COMMENTS
 
-# ===== History
-
-# Allow multiple terminal sessions to all append to one zsh command history
-setopt APPEND_HISTORY 
-
-# Add comamnds as they are typed, don't wait until shell exit
-setopt APPEND_HISTORY 
-
-# Do not write events to history that are duplicates of previous events
-setopt HIST_IGNORE_DUPS
-
-# When searching history don't display results already cycled through twice
-setopt HIST_FIND_NO_DUPS
-
-# Remove extra blanks from each command line being added to history
-setopt HIST_REDUCE_BLANKS
-
-# Include more information about when the command was executed, etc
-setopt EXTENDED_HISTORY
-
 # ===== Completion 
 
 # Allow completion from within a word/phrase
