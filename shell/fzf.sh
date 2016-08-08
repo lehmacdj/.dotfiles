6dcb517ee4cbec8df5ad6df6ce82c6e505df50cc
Devin Lehmacher <lehmacdj@gmail.com>
2016-08-08 10:00:51 -0400

    Make fzf config work on any platform

--- shell/fzf.sh
+++ shell/fzf.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 # Setup fzf
 if [[ ! "$PATH" == */Users/devin/.fzf/bin* ]]; then
-  export PATH="$PATH:/Users/devin/.fzf/bin"
+  export PATH="$PATH:$HOME/.fzf/bin"
 fi
 
 # Man path
-if [[ ! "$MANPATH" == */Users/devin/.fzf/man* && -d "/Users/devin/.fzf/man" ]]; then
-  export MANPATH="$MANPATH:/Users/devin/.fzf/man"
+if [[ ! "$MANPATH" == */Users/devin/.fzf/man* && -d "$HOME/.fzf/man" ]]; then
+  export MANPATH="$MANPATH:$HOME/.fzf/man"
 fi
 
 if [ -n "$BASH_VERSION" ]; then
     # Auto-completion
-    [[ $- == *i* ]] && source "/Users/devin/.fzf/shell/completion.bash" 2> /dev/null
+    [[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
 
     # Key bindings
-    source "/Users/devin/.fzf/shell/key-bindings.bash"
+    source "$HOME/.fzf/shell/key-bindings.bash"
 elif [ -n "$ZSH_VERSION" ]; then
     # Auto-completion
-    [[ $- == *i* ]] && source "/Users/devin/.fzf/shell/completion.zsh" 2> /dev/null
+    [[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
     # Key bindings
-    source "/Users/devin/.fzf/shell/key-bindings.zsh"
+    source "$HOME/.fzf/shell/key-bindings.zsh"
 fi
 
 # Set the default fzf command
