bd9481845b65cdb99b47655440f32ce31d033fff
Devin Lehmacher <lehmacdj@gmail.com>
2016-08-08 10:06:56 -0400

    Fix bug that made shell exit

--- shell/fzf.sh
+++ shell/fzf.sh
@@ -38,5 +38,5 @@ function vf () {
 # cd to directory using fzf
 function cf () {
   local dir
-  dir=$(find "${1:-.}" -type d 2> /dev/null | fzf +m) && cd "$dir" || exit
+  dir=$(find "${1:-.}" -type d 2> /dev/null | fzf +m) && cd "$dir"
 }
