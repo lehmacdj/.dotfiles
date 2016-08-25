fe85699e62376cb7bebae4bb58d0c0cb350c99d3
Devin Lehmacher <lehmacdj@gmail.com>
2016-08-25 18:51:19 -0400

    Add a templating manager to my dotfiles

---
+++ templates/c++/2024/main.cpp
@@ -0,0 +1,18 @@
+//
+// CS2024 -- Assignment <#Assignment number#>
+//
+// djl329 Devin Lehmacher  <#Date#>
+//
+// Exercise <#Excercise number#> Deitel & Deitel's "C++ How to Program"
+//
+// <#Description of the assignment and its purpose#>
+//
+// <#What I learned in this assignment etc#>
+//
+
+#include <iostream>
+
+int main (int argc, char *argv[]) {
+
+    return 0;
+}
