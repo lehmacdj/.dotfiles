186bb8bd3d97108142aa19768ce9e710bd10ad8f
Devin Lehmacher <lehmacdj@gmail.com>
2016-12-12 22:16:02 -0500

    Fix keybindings

--- zsh/inputrc.zsh
+++ zsh/inputrc.zsh
@@ -8,11 +8,15 @@ bindkey '^?' backward-delete-char
 # Make editor invoking work
 autoload edit-command-line
 zle -N edit-command-line
-bindkey -M vicmd v edit-command-line
-# Bind jk to escape
-bindkey -M viins 'jk' vi-cmd-mode
+bindkey -M viins '^F' edit-command-line
+bindkey -M vicmd '^F' edit-command-line
 # Make key timeout smaller so switch is faster
 export KEYTIMEOUT=10
 # Make ^R work as expected
 bindkey -M viins '^R' history-incremental-search-backward
 bindkey -M vicmd '^R' history-incremental-search-backward
+# Bind make C-j and C-k work correctly
+bindkey -M viins '^K' history-search-backward
+bindkey -M vicmd '^K' history-search-backward
+bindkey -M viins '^J' history-search-forward
+bindkey -M vicmd '^J' history-search-forward
