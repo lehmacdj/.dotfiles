bde8c2fda9961e9cf2b311199c646b3090e95b5c
Devin Lehmacher <lehmacdj@gmail.com>
2017-02-15 15:14:44 -0500

    Remove useless linuxbrew config

--- shell/env.sh
+++ shell/env.sh
@@ -33,14 +33,6 @@ if which brew >/dev/null 2>&1; then
         MANPATH="$BREW_PREFIX/opt/coreutils/libexec/gnuman:$MANPATH"
 fi
 
-if [ "$LINUX" ] && [ -d "$HOME/brew" ]; then
-    export PATH="$HOME/brew/bin:$PATH"
-    export CPATH="$HOME/brew/include:$CPATH"
-    export LD_LIBRARY_PATH="$HOME/brew/lib:$LD_LIBRARY_PATH"
-
-    [ -f "/etc/bash_completion" ] && source "/etc/bash_completion"
-fi
-
 # OCaml/OPAM configuration
 if [ -f "$HOME/.opam/opam-init/init.sh" ]; then
     source "$HOME/.opam/opam-init/init.sh" > /dev/null 2>&1 || true
