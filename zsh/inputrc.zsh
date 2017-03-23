932fc8a92bb2a49cf3c86d126ff5072c19898cf8
Devin Lehmacher <lehmacdj@gmail.com>
2017-03-22 20:38:05 -0400

    zsh: update keybindings

--- zsh/inputrc.zsh
+++ zsh/inputrc.zsh
@@ -1,22 +1,41 @@
 # Use vi-mode
 bindkey -v
 
-# backwards search through completions
+# S-tab to reverse scroll through completions
 bindkey -M menuselect '[Z' reverse-menu-complete
+
 # make backspace work
 bindkey '^?' backward-delete-char
-# Make editor invoking work
-autoload edit-command-line
+
+# invoke external editor
+autoload -Uz edit-command-line
 zle -N edit-command-line
-bindkey -M viins '^F' edit-command-line
-bindkey -M vicmd '^F' edit-command-line
+bindkey -M viins '^f' edit-command-line
+bindkey -M vicmd '^f' edit-command-line
+
+# incremental search
+bindkey -M viins '^r' history-incremental-search-backward
+bindkey -M vicmd '^r' history-incremental-search-backward
+
+# search forward and backward
+bindkey -M viins '^k' history-search-backward
+bindkey -M vicmd '^k' history-search-backward
+bindkey -M viins '^j' history-search-forward
+bindkey -M vicmd '^j' history-search-forward
+
+# go forward and backward in insert mode
+bindkey -M viins '^n' down-history
+bindkey -M viins '^p' up-history
+
+# surround commands
+autoload -Uz surround
+zle -N delete-surround surround
+zle -N add-surround surround
+zle -N change-surround surround
+bindkey -M vicmd cs change-surround
+bindkey -M vicmd ds delete-surround
+bindkey -M vicmd ys add-surround
+bindkey -M visual S add-surround
+
 # Make key timeout smaller so switch is faster
 export KEYTIMEOUT=10
-# Make ^R work as expected
-bindkey -M viins '^R' history-incremental-search-backward
-bindkey -M vicmd '^R' history-incremental-search-backward
-# Bind make C-j and C-k work correctly
-bindkey -M viins '^K' history-search-backward
-bindkey -M vicmd '^K' history-search-backward
-bindkey -M viins '^J' history-search-forward
-bindkey -M vicmd '^J' history-search-forward
