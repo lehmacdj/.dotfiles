1fa00f47a53faef848f723fb7f799b2f9bff6a9a
Devin Lehmacher <lehmacdj@gmail.com>
2019-04-23 18:11:23 -0400

    add settings properly

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -18,10 +18,8 @@ HISTSIZE=100000
 SAVEHIST=100000
 HISTFILE="$DOTFILES/zsh/.zhistory"
 HISTORY_IGNORE="(ls|ll|bg|fg|history)"
-# Allow multiple terminal sessions to all append to one zsh command history
-setopt append_history
 # Add comamnds as they are typed, don't wait until shell exit
-setopt append_history
+setopt inc_append_history
 # Include more information about when the command was executed, etc
 setopt extended_history
 # Do not write events to history that are duplicates of previous events
