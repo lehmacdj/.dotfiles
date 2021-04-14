ebf59fa8f18a4a21c0a62b2cc79fe0dbbda2b85d
Devin Lehmacher <lehmacdj@gmail.com>
2021-04-13 23:12:18 -0700

    change history settings

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -20,12 +20,12 @@ HISTFILE="$DOTFILES/zsh/.zhistory"
 HISTORY_IGNORE="(ls|ll|bg|fg|history)"
 # Add comamnds as they are typed, don't wait until shell exit
 setopt inc_append_history
+# Share history between zsh sessions (reading history file every time)
+setopt share_history
 # Include more information about when the command was executed, etc
 setopt extended_history
 # Do not write events to history that are duplicates of previous events
 setopt hist_ignore_dups
-# When searching history don't display results already cycled through twice
-setopt hist_find_no_dups
 # Remove extra blanks from each command line being added to history
 setopt hist_reduce_blanks
 
