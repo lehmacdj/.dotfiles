770ce0db6183abb9bfc709e56c1c2f071d034206
Devin Lehmacher <lehmacdj@gmail.com>
2021-11-07 15:42:24 -0800

    add some scripts that were in a computer specific bin

---
+++ bin/bitrate.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from mutagen.mp3 import MP3
+import sys
+
+if len(sys.argv) < 2:
+    print('error: didn\'t pass enough arguments')
+    print('usage: ./bitrate.py <file name>')
+    print('usage: find the bitrate of an mp3 file')
+    exit(1)
+
+f = MP3(sys.argv[1])
+print('bitrate: %s' % (f.info.bitrate / 1000))
