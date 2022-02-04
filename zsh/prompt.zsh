448998b3bb7785d45fdae324e3ddfd4ccbb1c7aa
Devin Lehmacher <lehmacdj@gmail.com>
2022-02-03 23:09:35 -0800

    update prompt to work with both right prompt/not

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,4 +1,6 @@
 if starship help >/dev/null 2>&1; then
+  "$DOTFILES/starship/build-starship.sh"
+  export STARSHIP_CONFIG="$DOTFILES/starship/right_prompt.starship.toml"
   eval "$(starship init zsh)"
 else
   export PROMPT="%~> "
