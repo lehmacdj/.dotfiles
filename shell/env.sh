b3e9f95eadac8d15c99b3ddaa33c4b46a9a93ad5
Devin Lehmacher <lehmacdj@gmail.com>
2022-10-11 17:01:43 -0700

    improve fpath addition

--- shell/env.sh
+++ shell/env.sh
@@ -101,8 +101,8 @@ if test "$EDITOR" = 'nvim'; then
 fi
 
 # fpath is a path for files containing function definitions; this includes
-# completion functions, and it must be set before compinit is called
-# (most likely by antigen)
+# completion functions for zsh, and it must be set before compinit is called
+# (most likely by antigen).
 cond_fpath_add () {
     [ -n "$1" ] || { echo "cond_fpath_add requires 1 argument"; exit 1; }
     [ -d "$1" ] && FPATH="$1:$FPATH"
@@ -110,6 +110,6 @@ cond_fpath_add () {
 
 cond_fpath_add "$HOME/.local/share/zsh-completions"
 cond_fpath_add /usr/local/share/zsh-completions
-cond_fpath_add /opt/homebrew/share/zsh-completions
+cond_fpath_add "$(brew --prefix)/share/zsh-completions"
 FPATH="$(consolidate-path "$FPATH")"
 export FPATH
