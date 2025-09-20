c8a1ba03ad2621c22ffe9dfe81666531ce7df2ea
Devin Lehmacher <lehmacdj@gmail.com>
2025-09-20 15:02:15 -0400

    make ouput kitty quick-access-terminal screen dynamic

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,6 +1,15 @@
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
-hs.hotkey.bind({"cmd", "ctrl"}, "return", function()
-  hs.task.new("/opt/homebrew/bin/kitten", nil, {"quick-access-terminal", "--detach"}):start()
+hs.hotkey.bind({"control"}, "return", function()
+  hs.task.new(
+    "/opt/homebrew/bin/kitten",
+    nil,
+    {
+      "quick-access-terminal",
+      "--detach",
+      "--override",
+      "output_name="..hs.mouse.getCurrentScreen():name(),
+    }
+  ):start()
 end)
