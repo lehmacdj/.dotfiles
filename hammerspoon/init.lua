7aaf9fc6cae283d69b3edfcca66dddb7e7a68781
Devin Lehmacher <lehmacdj@gmail.com>
2025-09-21 22:55:31 -0400

    Fix quick-access-terminal hotkey

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,15 +1,25 @@
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
-hs.hotkey.bind({"control"}, "return", function()
-  hs.task.new(
+local start_kitty_quick_access_terminal = function()
+  local kitty_quick_access_app = hs.application.get("kitty-quick-access")
+  local options =  {
+    "quick-access-terminal",
+    "--detach",
+  }
+  if kitty_quick_access_app then
+    table.insert(options, "--override")
+    table.insert(options, "output_name="..hs.mouse.getCurrentScreen():name())
+  end
+  local task = hs.task.new(
     "/opt/homebrew/bin/kitten",
     nil,
-    {
-      "quick-access-terminal",
-      "--detach",
-      "--override",
-      "output_name="..hs.mouse.getCurrentScreen():name(),
-    }
-  ):start()
+    options
+  )
+  task:setWorkingDirectory(os.getenv("HOME"))
+  task:start()
+end
+
+hs.hotkey.bind({"control"}, "return", function()
+  start_kitty_quick_access_terminal()
 end)
