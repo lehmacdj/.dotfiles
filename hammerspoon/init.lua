008e545c393a56f03141c75d2fb37e16f0cf99b7
Devin Lehmacher <lehmacdj@gmail.com>
2025-09-22 10:53:30 -0400

    explain complex quick-access-terminal setup

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,11 +2,17 @@ hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
 local start_kitty_quick_access_terminal = function()
+  -- the kitty-quick-access app is a reliable way to detect whether the quick
+  -- access terminal is already running
   local kitty_quick_access_app = hs.application.get("kitty-quick-access")
   local options =  {
     "quick-access-terminal",
     "--detach",
   }
+  -- I've found that if --override output_name is specified the first time
+  -- the quick access terminal is launched, it gets stuck on some screen, but
+  -- if it is already running, output_name reliably causes the terminal to
+  -- switch to the specified screen
   if kitty_quick_access_app then
     table.insert(options, "--override")
     table.insert(options, "output_name="..hs.mouse.getCurrentScreen():name())
