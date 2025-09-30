fc10e6b4fc33166d23c44f65fde754050533c3b9
Devin Lehmacher <lehmacdj@gmail.com>
2025-09-30 00:25:56 -0400

    remove iterm settings, explain vlc settings better

--- install_keybindings.macos.sh
+++ install_keybindings.macos.sh
@@ -2,17 +2,6 @@
 # $ means shift so in this file we want to ignore 2016
 # shellcheck disable=2016
 
-defaults write com.googlecode.iterm2 NSUserKeyEquivalents '
-{
-    "New Tab with Current Profile" = "@t";
-    "Quit and Keep Windows" = "@q";
-    "Select Pane Above" = "@$k";
-    "Select Pane Below" = "@$j";
-    "Select Pane Left" = "@$h";
-    "Select Pane Right" = "@$l";
-}
-'
-
 defaults write com.tinyspeck.slackmacgap NSUserKeyEquivalents '
 {
     Back = "^o";
@@ -28,7 +17,7 @@ defaults write com.apple.iphonesimulator NSUserKeyEquivalents '
 
 # strangely there isn't a default keybinding
 # command control l is unbound by default and matches the default commands for
-# loop / A-B loop
+# loop (@l) / A-B loop (@$l)
 defaults write org.videolan.vlc NSUserKeyEquivalents '
 {
     "Repeat One" = "@^l";
