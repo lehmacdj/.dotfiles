85e51468a59c8f0e6d585bcbdd5fad04160ed7ff
Devin Lehmacher <lehmacdj@gmail.com>
2025-11-20 14:26:16 -0500

    add C-y fzf widget for jj history

--- shell/fzf.sh
+++ shell/fzf.sh
@@ -26,6 +26,16 @@ __fzf_select_git__() {
   return $ret
 }
 
+__fzf_jj_change__() {
+  FZF_DEFAULT_COMMAND="jj log -r 'mutable()' --color=always | perl -pe 's/^(│.*)\n/\$1\0/g'" \
+  FZF_DEFAULT_OPTS=$(__fzf_defaults "--reverse --ansi" "--read0 --tiebreak=index -m") \
+  FZF_DEFAULT_OPTS_FILE='' $(__fzfcmd) "$@" < /dev/tty | \
+  perl -0 -ne 'if (/([a-z]{8})/) { print "$1 " }'
+  local ret=$?
+  echo
+  return $ret
+}
+
 if [ -n "$BASH_VERSION" ] && [[ $- =~ i ]]; then
   fzf-git-widget() {
     local selected
@@ -49,6 +59,31 @@ elif [ -n "$ZSH_VERSION" ] && [[ -o interactive ]]; then
   bindkey -M emacs '^G' fzf-git-widget
   bindkey -M vicmd '^G' fzf-git-widget
   bindkey -M viins '^G' fzf-git-widget
+fi
+
+if [ -n "$BASH_VERSION" ] && [[ $- =~ i ]]; then
+  fzf-jj-change-widget() {
+    local selected
+    selected="$(__fzf_jj_change__ "$@")"
+    READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}$selected${READLINE_LINE:$READLINE_POINT}"
+    READLINE_POINT=$(( READLINE_POINT + ${#selected} ))
+  }
+
+  bind -m emacs-standard -x '"\C-y": fzf-jj-change-widget'
+  bind -m vi-command -x '"\C-y": fzf-jj-change-widget'
+  bind -m vi-insert -x '"\C-y": fzf-jj-change-widget'
+elif [ -n "$ZSH_VERSION" ] && [[ -o interactive ]]; then
+  fzf-jj-change-widget() {
+     LBUFFER="${LBUFFER}$(__fzf_jj_change__)"
+     local ret=$?
+     zle reset-prompt
+     return $ret
+  }
+
+  zle     -N            fzf-jj-change-widget
+  bindkey -M emacs '^Y' fzf-jj-change-widget
+  bindkey -M vicmd '^Y' fzf-jj-change-widget
+  bindkey -M viins '^Y' fzf-jj-change-widget
 fi
 
 # cd to directory using fzf
