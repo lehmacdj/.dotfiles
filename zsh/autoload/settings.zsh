ca290eb201a23806c52bfaf695b6da3fc53b2ece
Devin Lehmacher <lehmacdj@gmail.com>
2026-05-11 13:34:11 -0400

    Optimize history settings

--- zsh/autoload/settings.zsh
+++ zsh/autoload/settings.zsh
@@ -20,16 +20,6 @@ autoload -Uz bracketed-paste-url-magic
 zle -N bracketed-paste bracketed-paste-url-magic
 
 # HISTORY
-HISTSIZE=10000000
-SAVEHIST=10000000
-HISTFILE="$DOTFILES/zsh/.zhistory"
-HISTORY_IGNORE="(ls|ll|bg|fg)"
-# https://unix.stackexchange.com/questions/562722/ignore-history-when-using-zsh
-function zshaddhistory {
-    emulate -L zsh
-    setopt extendedglob
-    [[ $1 != ${~HISTORY_IGNORE} ]]
-}
 # Share history between zsh sessions (reading history file every time)
 # this implies inc_append_history and shouldn't be set together with it
 # apparently
@@ -40,6 +30,20 @@ setopt extended_history
 setopt hist_ignore_dups
 # Remove extra blanks from each command line being added to history
 setopt hist_reduce_blanks
+# Use fcntl locking when writing history; safer with concurrent shells
+setopt hist_fcntl_lock
+HISTSIZE=10000000
+SAVEHIST=10000000
+HISTFILE="${XDG_STATE_HOME:-$HOME/.local/state}/zsh/history"
+HISTORY_IGNORE="(ls|ll|bg|fg)"
+# this causes HISTORY_IGNORE to be applied whenever writing
+# thus adding to history ignore can delete entires that were previously saved
+# https://unix.stackexchange.com/questions/562722/ignore-history-when-using-zsh
+function zshaddhistory {
+    emulate -L zsh
+    setopt extendedglob
+    [[ $1 != ${~HISTORY_IGNORE} ]]
+}
 
 # autoload plugins
 autoload -U zmv
